133e719d6d2477423d9556861682cc5d9afe0407
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2020-09-25 13:08:41 +0200

    Updates

---
+++ precache-manifest.c9552fb33bb6ab5535fc7b2ce5e66a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaa1cd6740b2861b3d7ec48cdecfa09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78659d3f15adf2301d7b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc0108e8713d8cbab107",
+    "url": "/static/js/2.e8417e3b.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.e8417e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78659d3f15adf2301d7b",
+    "url": "/static/js/main.8f15ec32.chunk.js"
+  },
+  {
+    "revision": "1ad8a54f373e62eddbff",
+    "url": "/static/js/runtime-main.8b43c8eb.js"
+  }
+]);
